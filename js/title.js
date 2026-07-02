fd3da7daca6c7b34a2cb848669e5937924940cf3
wyin404 <wyin5100@outlook.com>
2026-07-03 06:47:22 +0800

    Site updated: 2026-07-03 06:47:21

---
+++ js/title.js
@@ -0,0 +1,15 @@
+(function() {
+  var normalTitle = document.title;
+  var hiddenTitle = '不许走要永远陪着我 :( 我恨你';
+
+  document.addEventListener('visibilitychange', function() {
+    document.title = document.hidden ? hiddenTitle : normalTitle;
+  });
+
+  window.addEventListener('blur', function() {
+    document.title = hiddenTitle;
+  });
+  window.addEventListener('focus', function() {
+    document.title = normalTitle;
+  });
+})();
