00e38c20178ed98d19a257723ea2145f2ad92075
wyin404 <wyin5100@outlook.com>
2026-07-06 00:34:40 +0800

    Site updated: 2026-07-06 00:34:40

--- js/title.js
+++ js/title.js
@@ -1,6 +1,6 @@
 (function() {
   var normalTitle = document.title;
-  var hiddenTitle = '喵呜~主人~快回来呀~';
+  var hiddenTitle = '喵呜~主人不要我了?';
 
   document.addEventListener('visibilitychange', function() {
     document.title = document.hidden ? hiddenTitle : normalTitle;
